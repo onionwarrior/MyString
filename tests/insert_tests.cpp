d8520593af63412da926032497b41b7920118eec
onionwarrior <onionwarrior@mail.ru>
2021-09-16 02:09:22 +0300

    some

---
+++ tests/insert_tests.cpp
@@ -0,0 +1,10 @@
+#include "../MyString.hpp"
+#include <assert.h>
+void are_equal(const char *a,const char*b)
+{
+    assert(strcmp(a,b)==0);
+}
+void do_insert_tests()
+{
+    
+}
--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -1,5 +1,7 @@
 #include "constructor_tests.cpp"
+#include "insert_tests.cpp"
 int main()
 {
     do_constructor_tests();
+    do_insert_tests();
 }
