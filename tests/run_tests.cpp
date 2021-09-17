bac2c6b4990f99b5019f9c90045a373157d1f1fa
onionwarrior <onionwarrior@mail.ru>
2021-09-17 19:24:40 +0300

    empty

--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -3,11 +3,13 @@
 #include "replace_tests.cpp"
 #include "insert_tests.cpp"
 #include "substr_tests.cpp"
+#include "find_tests.cpp"
 int main()
 {
-    //do_constructor_tests();
-    //do_append_tests();
-    //do_replace_tests();
-    //do_insert_tests();
+    do_constructor_tests();
+    do_append_tests();
+    do_replace_tests();
+    do_insert_tests();
     do_substr_tests();
+    do_find_tests();
 }
