d931f1145f6c61547562645e7cd71fd899ff2919
onionwarrior <onionwarrior@mail.ru>
2021-09-18 02:19:43 +1000

    find tests

--- tests/find_tests.cpp
+++ tests/find_tests.cpp
@@ -1,5 +1,13 @@
 #include "testing_utilites.hpp"
 void do_find_tests()
 {
-    
+    MyString test_string("lorem ipsum");
+    assert(test_string.find("lorem ipsum")==0);
+    assert(test_string.find("ipsum lorem")==-1);
+    assert(test_string.find("i")==6);
+    assert(test_string.find("m")==4);
+    assert(test_string.find("m",5)==10);
+    assert(test_string.find("lorem")==0);
+    assert(test_string.find("rem")==2);
+    assert(test_string.find("sum")==8);
 }
